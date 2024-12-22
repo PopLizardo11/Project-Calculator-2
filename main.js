2d06062b70dab08238eef442bedc2ffb33c2a327
pop <poplizardo4321@gmail.com>
2024-12-22 14:26:04 +0800

    Added basic operate function single chain

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const display = document.querySelector("#display");
 const numberButtons = document.querySelectorAll(".numberButton");
+const opButtons = document.querySelectorAll(".opButton");
 const clearButton = document.querySelector("#clearButton");
 
-let firstNum;
-let operator;
-let nextNum; 
+let firstNum = 0;
+let operator = "+";
+let nextNum = false; 
+let toNext = false;
 
 function add(a, b) {
     return a + b
@@ -35,12 +37,41 @@ function operate(x, y, op){
     }
 }
 
-numberButtons.forEach((node) => node.addEventListener("click", () => {
+numberButtons.forEach((btn) => btn.addEventListener("click", () => {
     if (display.textContent !== "") {
-        display.textContent += node.textContent;
+        display.textContent += btn.textContent;
     } else {
-        display.textContent = node.textContent;    
+        display.textContent = btn.textContent;   
     }
+
+    if (toNext) {
+        nextNum = parseFloat(display.textContent);
+        console.log(`${firstNum}, ${nextNum}`)
+    } else {
+        firstNum = parseFloat(display.textContent);
+        console.log(`${firstNum}, ${nextNum}`)
+    } 
 }))
 
-clearButton.addEventListener("click", () => display.textContent = "");
+opButtons.forEach((btn) => btn.addEventListener("click", ()=> {
+    operator = btn.getAttribute("data-key")
+    display.textContent = ""
+    toNext = true;
+    if (nextNum === false) {
+        return
+    }
+    if (nextNum === 0 && operator === "/") {
+        alert("Dividing by zero is not advised")
+        return
+    }
+    alert(operate(firstNum, nextNum, operator))
+}))
+
+equalButton.addEventListener("click", () => {})
+
+clearButton.addEventListener("click", () => {
+    display.textContent = ""
+    firstNum = 0;
+    operator = "+";
+    nextNum = 0;
+});
